3738f9fdac1fd0d719033c3d06271a7d1e188d59
simlay <simlay@localhost>
2018-06-10 13:25:45 -0700

    Updated tile dragging

--- src/game.rs
+++ src/game.rs
@@ -33,15 +33,15 @@ use std::cmp::{
 };
 use stdweb::web::Date;
 
-pub const TILE_WIDTH : usize = 80;
-pub const TILE_HEIGHT : usize = 80;
+pub const TILE_WIDTH  : i32 = 80;
+pub const TILE_HEIGHT : i32 = 80;
 
-pub const GRID_WIDTH : usize = 7;
-pub const GRID_HEIGHT : usize = 8;
+pub const GRID_WIDTH  : i32 = 7;
+pub const GRID_HEIGHT : i32 = 8;
 
 
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Tile {
     x: i32,
     y: i32,
@@ -58,6 +58,12 @@ impl Tile {
             value: value,
         }
     }
+    pub fn inside(&self, x: i32, y: i32) -> bool {
+        x > self.x - TILE_WIDTH/2 &&
+        x < self.x + TILE_WIDTH/2 &&
+        y > self.y - TILE_HEIGHT/2 &&
+        y < self.y + TILE_HEIGHT/2
+    }
 }
 
 fn get_image(value: i8) -> ImageElement {
@@ -66,17 +72,20 @@ fn get_image(value: i8) -> ImageElement {
     image
 }
 
-pub struct Game {
-    //tiles: Vec<Tile>,
-    images: HashMap<i8, ImageElement>,
-    context: CanvasRenderingContext2d,
-    grid: [[i8; GRID_HEIGHT as usize]; GRID_WIDTH as usize],
-    drag_tile: Option<Tile>,
-    time_old: f64,
-    air_tiles: Vec<Tile>,
+struct TimeBar {
+    percent: f32,
 }
 
-struct TimeBar {
+static LOAD_TIMEOUT: f64 = 5000.0;
+
+pub struct Game {
+    images: HashMap<i8, ImageElement>,
+    context: CanvasRenderingContext2d,
+    drag_tile: Option<Tile>,
+    time_old: f64,
+    count_down: f64,
+    tiles: Vec<Tile>,
+    //air_tiles: Vec<Tile>,
 }
 
 impl Game {
@@ -89,10 +98,11 @@ impl Game {
         Game {
             images: images,
             context: context,
-            grid: [[0; GRID_HEIGHT]; GRID_WIDTH],
             drag_tile: None,
             time_old: 0.0,
-            air_tiles: Vec::new(),
+            count_down: LOAD_TIMEOUT,
+            tiles: Vec::new(),
+            //air_tiles: Vec::new(),
         }
     }
 
@@ -107,180 +117,202 @@ impl Game {
     }
 
     fn new_row(&mut self) {
-        for x in 0..GRID_WIDTH {
-            self.grid[x][GRID_HEIGHT - 2] = self.grid[x][GRID_HEIGHT - 1];
-        }
 
         let date = Date::now();
-        for x in 0..GRID_WIDTH {
-            let random = (date*date)*(x as f64 + 1.0) % 5.0;
-            self.grid[x][GRID_HEIGHT - 1] = random as i8 + 1;
+
+        for y in 0..=0 {
+            for x in 0..=GRID_WIDTH {
+                let random = (date*date)*(y as f64 + 1.0)*(x as f64 + 1.0) % 5.0;
+
+                self.tiles.push(Tile::new(
+                        x*TILE_WIDTH - TILE_WIDTH/2,
+                        (GRID_HEIGHT - y)* TILE_HEIGHT - TILE_HEIGHT/2,
+                        0 as i8 + 1));
+            }
         }
     }
     pub fn draw(&self) {
-        /*
-        self.context.clear_rect(
-            0.0,
-            0.0,
-            (TILE_WIDTH as f64)*(GRID_WIDTH as f64),
-            (TILE_HEIGHT as f64)*(GRID_HEIGHT as f64),
-        );
-        */
         self.context.fill_rect(
             0.0,
             0.0,
-            (TILE_WIDTH as f64)*(GRID_WIDTH as f64),
-            (TILE_HEIGHT as f64)*(GRID_HEIGHT as f64),
+            (TILE_WIDTH * GRID_WIDTH) as f64,
+            (TILE_HEIGHT * GRID_HEIGHT) as f64,
         );
-        for x in 0..GRID_WIDTH {
-            for y in 0..GRID_HEIGHT {
-                //console!(log, "GRID: ", x as i8, y as i8, self.grid[x][y]);
-                match self.grid[x][y] {
-                    0 => {
-                    },
-                    value => {
-                        self.context.draw_image(
-                            self.images.get(&value).unwrap().clone(),
-                            (x as i32 *TILE_WIDTH as i32) as f64,
-                            (y as i32 *TILE_HEIGHT as i32) as f64,
-                            ).unwrap();
-                    }
-                }
-            }
-        };
-        {
-            match self.drag_tile {
-                Some(ref tile) => {
-                    self.context.draw_image(
-                        self.images.get(&tile.value).unwrap().clone(),
-                        (tile.x - (TILE_WIDTH as i32)/2) as f64,
-                        (tile.y - (TILE_HEIGHT as i32)/2) as f64,
-                        ).unwrap();
-                },
-                None => {},
-            }
+        match self.drag_tile {
+            Some(ref tile) => {
+                self.context.draw_image(
+                    self.images.get(&tile.value).unwrap().clone(),
+                    (tile.x - TILE_WIDTH /2) as f64,
+                    (tile.y - TILE_HEIGHT/2) as f64,
+                    ).unwrap();
+            },
+            None => {},
         }
-        for air_tile in self.air_tiles.iter() {
+        for tile in self.tiles.iter() {
             self.context.draw_image(
-                self.images.get(&air_tile.value).unwrap().clone(),
-                (air_tile.x - (TILE_WIDTH as i32)/2) as f64,
-                (air_tile.y - (TILE_HEIGHT as i32)/2) as f64,
+                self.images.get(&tile.value).unwrap().clone(),
+                (tile.x - TILE_WIDTH/2) as f64,
+                (tile.y - TILE_HEIGHT/2) as f64,
                 ).unwrap();
         }
     }
 
-    fn take_tile(&self, x: i32, y: i32) -> Option<Tile> {
-        let (x_tile, y_tile) = self.get_tile_coordinates(x, y);
-        match self.grid[x_tile][y_tile] {
-            0 => {
-                None
-                /*
-                 * TODO: Make it so one can pick up the tile again.
-                match self.air_tiles.iter().filter(|tile| {
-                    x > 1
-                }).next().take() {
-                    None => {
-                        None,
-                    }
-                    Some(tile) => {
-                        Some(tile)
-                    }
-                }
-                */
-            },
-            value => {
-                Some(Tile::new(x, y, value))
-            }
-        }
-    }
-    fn get_tile_coordinates(&self, x: i32, y: i32) -> (usize, usize) {
+    fn get_tile_coordinates(&self, x: i32, y: i32) -> (i32, i32) {
         (
-            min(max(x / TILE_WIDTH as i32, 0), GRID_WIDTH as i32 - 1) as usize,
-            min(max(y / TILE_HEIGHT as i32, 0), GRID_HEIGHT as i32 - 1) as usize
+            min(max(x / TILE_WIDTH , 0), GRID_WIDTH  - 1),
+            min(max(y / TILE_HEIGHT, 0), GRID_HEIGHT - 1)
         )
     }
 
-    pub fn mouse_down(&mut self, event: MouseDownEvent) {
-        self.drag_tile = self.take_tile(event.client_x(), event.client_y());
-        match self.drag_tile {
-            Some(ref tile) => {
-                let (x_tile, y_tile) = self.get_tile_coordinates(
-                    tile.x,
-                    tile.y
-                );
-                self.grid[x_tile][y_tile] = 0;
-            },
-            None => {
-            },
-        }
-    }
 
-    fn collision(&self, center_x: i32, center_y: i32) -> bool {
-        let tile_height = TILE_HEIGHT as i32 - 10;
-        let tile_width = TILE_WIDTH as i32 - 10;
+    fn collision(&self, center_x: i32, center_y: i32, value: i8) -> bool {
+        let padding = 5;
         let corners = [
-            (center_x - (tile_width/2), center_y - (tile_height/2)),
-            (center_x + (tile_width/2), center_y + (tile_height/2)),
-            (center_x - (tile_width/2), center_y + (tile_height/2)),
-            (center_x + (tile_width/2), center_y - (tile_height/2)),
+            (center_x - TILE_WIDTH/2 + padding, center_y - TILE_HEIGHT/2),
+            (center_x + TILE_WIDTH/2 - padding, center_y + TILE_HEIGHT/2),
+            (center_x - TILE_WIDTH/2 + padding, center_y + TILE_HEIGHT/2),
+            (center_x + TILE_WIDTH/2 - padding, center_y - TILE_HEIGHT/2),
         ];
 
+        let max_x = TILE_WIDTH * GRID_WIDTH;
+        let max_y = TILE_HEIGHT * GRID_HEIGHT;
+
         let mut collided = false;
-        let max_x : i32 = TILE_WIDTH as i32 * GRID_WIDTH as i32 + 10;
-        let max_y : i32 = TILE_HEIGHT as i32 * GRID_HEIGHT as i32 + 10;
-
-        //console!(log, center_x, center_y);
-
         for (x, y) in corners.iter() {
-            let (x_tile, y_tile) = self.get_tile_coordinates(*x, *y);
-            if self.grid[x_tile][y_tile] != 0 ||
-                *x < 0 ||
-                *y < 0 ||
-                *x > max_x ||
-                *y > max_y
-                {
+            if  *x < 0 || *y < 0 || *x > max_x || *y > max_y {
+                collided = true;
+                break;
+            }
+            for tile in self.tiles.iter() {
+                if tile.inside(*x, *y) && tile.value != value {
                     collided = true;
                     break;
                 }
+            }
         }
         collided
+    }
+
+    fn get_tile(&self, x: i32, y: i32) -> Option<&Tile> {
+        self.tiles.iter().filter(|tile| {
+            tile.inside(x, y)
+        }).next()
+    }
+
+    pub fn mouse_down(&mut self, event: MouseDownEvent) {
+        if self.drag_tile.is_none() {
+            self.drag_tile = match self.get_tile(event.client_x(), event.client_y()) {
+                Some(tile) => {
+                    Some(Tile::new(tile.x, tile.y, tile.value))
+                },
+                None => {
+                    None
+                },
+            };
+            self.tiles = self.tiles.iter().filter(|tile| {
+                !tile.inside(event.client_x(), event.client_y())
+            }).map(|tile| {
+                Tile::new(tile.x, tile.y, tile.value)
+            }).collect();
+        }
     }
 
     pub fn mouse_move(&mut self, event: MouseMoveEvent) {
         self.drag_tile = match self.drag_tile.take() {
             Some(mut tile) => {
-                if !self.collision(event.client_x(), event.client_y()) {
-                    tile.x = event.client_x();
-                    tile.y = event.client_y();
+                let mouse_x = event.client_x();
+                let mouse_y = event.client_y();
+
+                let mut new_x = tile.x;
+                let mut new_y = tile.y;
+
+                if tile.x < mouse_x {
+                    for x in tile.x..=mouse_x {
+                        if !self.collision(x, tile.y, tile.value) {
+                            new_x = x;
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    for x in (mouse_x..=tile.x).rev() {
+                        if !self.collision(x, tile.y, tile.value) {
+                            new_x = x;
+                        } else {
+                            break;
+                        }
+                    }
                 }
+                tile.x = new_x;
+
+                if tile.y < mouse_y {
+                    for y in tile.y..=mouse_y {
+                        if !self.collision(tile.x, y, tile.value) {
+                            new_y = y;
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    for y in (mouse_y..=tile.y).rev() {
+                        if !self.collision(tile.x, y, tile.value) {
+                            new_y = y;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+
+
+                tile.y = new_y;
+
+                /*
+                if !self.collision(mouse_x, mouse_y, tile.value) {
+                    tile.x = mouse_x;
+                    tile.y = mouse_y;
+                } else if !self.collision(mouse_x, tile.y, tile.value) {
+                    tile.x = mouse_x;
+                } else if !self.collision(tile.y, event.client_y(), tile.value) {
+                    tile.y = event.client_y()
+                }
+                */
                 Some(tile)
             },
             None => {
                 None
             }
-        }
+        };
     }
 
-    pub fn mouse_up(&mut self, _event: MouseUpEvent) {
-        match self.drag_tile.take() {
-            Some(mut tile) => {
-                let (x_tile, y_tile) = self.get_tile_coordinates(tile.x, tile.y);
-
-                if self.grid[x_tile][y_tile] == tile.value {
-                    self.grid[x_tile][y_tile] += 1;
-                } else {
-                    match self.grid[x_tile][y_tile] {
-                        0 => {
-                            //self.grid[x_tile][y_tile] = tile.value;
-                            // This is for air tiles
-                            let (tile_x, _) = self.get_tile_coordinates(tile.x, tile.y);
-                            tile.x = (tile_x as i32)*(TILE_WIDTH as i32) + (TILE_WIDTH as i32)/2;
-                            self.air_tiles.push(tile);
-                        },
-                        _ => {
-                        }
-                    }
+    fn join_tiles(&mut self, new_tile: Tile) {
+        self.tiles = self.tiles.iter().map(|tile| {
+            match tile.inside(new_tile.x, new_tile.y) && new_tile.value == tile.value {
+                true => {
+                    Tile::new(tile.x, tile.y, tile.value + 1)
+                },
+                false => {
+                    Tile::new(tile.x, tile.y, tile.value)
                 }
+            }
+        }).collect();
+
+
+        if self.tiles.iter().filter(|tile| {
+            tile.inside(new_tile.x, new_tile.y)
+        }).count() == 0 {
+            self.tiles.push(new_tile);
+        };
+    }
+
+
+
+    pub fn mouse_up(&mut self, _event: MouseUpEvent) {
+
+        match self.drag_tile.take() {
+            Some(mut drag_tile) => {
+                let (tile_x, _) = self.get_tile_coordinates(drag_tile.x, drag_tile.y);
+                drag_tile.x = tile_x*TILE_WIDTH  + TILE_WIDTH/2;
+                self.join_tiles(drag_tile);
             },
             None => {
             },
@@ -292,39 +324,31 @@ impl Game {
 
         let delta_y = 5;
 
-        for tile in self.air_tiles.iter() {
-            match self.collision(
-                tile.x,
-                tile.y  + delta_y,
-            ) {
-                true => {
-                    let (x_tile, y_tile) = self.get_tile_coordinates(tile.x, tile.y);
-                    if self.grid[x_tile][min(y_tile + 1, GRID_HEIGHT - 1)] == tile.value {
-                        self.grid[x_tile][min(y_tile + 1, GRID_HEIGHT - 1)] += 1;
-                    } else if self.grid[x_tile][y_tile] == 0 {
-                        self.grid[x_tile][y_tile] = tile.value;
-                    }
-                },
-                false => {}
-
-            }
-        }
-
-        self.air_tiles = self.air_tiles.iter()
-            .filter(|tile| {
-            !self.collision(
+        self.tiles = self.tiles.iter().map(|tile| {
+            if !self.collision(
              tile.x,
              tile.y + delta_y,
-            ) && tile.y < TILE_HEIGHT as i32 * GRID_HEIGHT as i32
-        })
-        .map(|tile| {
-            Tile::new(tile.x, tile.y + delta_y, tile.value)
+             tile.value,
+            ) {
+                Tile::new(tile.x, tile.y + delta_y, tile.value)
+            } else {
+                Tile::new(tile.x, tile.y, tile.value)
+            }
         }).collect();
     }
 
     pub fn animate(&mut self, time: f64, rc: Rc<RefCell<Self>>) {
-        let _dt = (time - self.time_old) as f32;
+        /*
+        let dt = time - self.time_old;
+        self.time_old = time;
+        self.count_down -= dt;
+        if self.count_down < 0.0 {
+            self.count_down = LOAD_TIMEOUT;
+            self.new_row();
+        }
+        */
         self.drop_tiles();
+
 
         self.draw();
         {
